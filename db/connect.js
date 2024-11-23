2bd4a78df2c03311ff7eb80aa857094667309052
Haileyesus Chekol <teamdevs7@gmail.com>
2024-11-23 14:20:00 +0000

    connect to db

--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,11 @@
+require('dotenv').config()
+
 const mongoose = require('mongoose')
 
-const connectDB = (url) => {
-  return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
+const connectDB = () => {
+   mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log('Db connected Successfully!')
+  }).catch((error)=>console.log(error))
 }
 
 module.exports = connectDB
