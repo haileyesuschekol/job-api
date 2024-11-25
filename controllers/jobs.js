75e566d34b0f1e61d5665a88a0585c1c6da991c9
Haileyesus Chekol <teamdevs7@gmail.com>
2024-11-25 10:36:32 +0000

    delete job

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -49,7 +49,17 @@ const updateJob = async (req, res) => {
 }
 
 const deleteJob = async (req, res) => {
-  res.send("delete job")
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req
+
+  const job = await Job.findOneAndDelete({ _id: jobId, createdBy: userId })
+  if (!job) {
+    throw new NotFoundError("Job not found!")
+  }
+
+  res.status(StatusCodes.OK).json({ msg: "Jon Deleted Successfully!" })
 }
 
 module.exports = { getAllJobs, getJob, createJob, updateJob, deleteJob }
