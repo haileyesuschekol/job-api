46b7cf88fd9bcec8bc63c66cc73bdadc5381f780
Haileyesus Chekol <teamdevs7@gmail.com>
2024-11-25 22:04:23 +0000

    make errors user friendly

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -7,59 +7,84 @@ const getAllJobs = async (req, res) => {
 }
 
 const getJob = async (req, res) => {
-  const {
-    user: { userId },
-    params: { id: jobId },
-  } = req
+  try {
+    const {
+      user: { userId },
+      params: { id: jobId },
+    } = req
 
-  const job = await Job.find({ _id: jobId, createdBy: userId })
-  if (!job) {
-    throw new NotFoundError("Job not found!")
+    const job = await Job.find({ _id: jobId, createdBy: userId })
+    if (!job) {
+      throw new NotFoundError("Job not found!")
+    }
+    res.status(StatusCodes.OK).json({ job })
+  } catch (error) {
+    if (error.name === "CastError") {
+      res.status(400).json({ msg: "Invalid request!" })
+    }
   }
-
-  res.status(StatusCodes.OK).json({ job })
 }
 
 const createJob = async (req, res) => {
-  req.body.createdBy = req.user.userId
-  const job = await Job.create(req.body)
-  res.status(StatusCodes.CREATED).json({ job })
+  try {
+    req.body.createdBy = req.user.userId
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({ job })
+  } catch (error) {
+    res.status(400).json({ msg: error.message })
+  }
 }
 const updateJob = async (req, res) => {
-  const {
-    body: { company, position },
-    user: { userId },
-    params: { id: jobId },
-  } = req
+  try {
+    const {
+      body: { company, position },
+      user: { userId },
+      params: { id: jobId },
+    } = req
 
-  if (!company || !position) {
-    throw new BadRequestError("inputs cannot be empty!")
+    if (!company || !position) {
+      throw new BadRequestError("Please Provide Valid Input!")
+    }
+
+    const job = await Job.findByIdAndUpdate(
+      { _id: jobId, createdBy: userId },
+      req.body,
+      { new: true, runValidators: true }
+    )
+
+    if (!job) {
+      throw new NotFoundError("Job not found!")
+    }
+    res.status(StatusCodes.OK).json({ job })
+  } catch (error) {
+    if (error.name === "CastError") {
+      res.status(400).json({ msg: "Invalid request!" })
+    } else {
+      res.status(400).json({ msg: error.message })
+    }
   }
-
-  const job = await Job.findByIdAndUpdate(
-    { _id: jobId, createdBy: userId },
-    req.body,
-    { new: true, runValidators: true }
-  )
-
-  if (!job) {
-    throw new NotFoundError("Job not found!")
-  }
-  res.status(StatusCodes.OK).json({ job })
 }
 
 const deleteJob = async (req, res) => {
-  const {
-    user: { userId },
-    params: { id: jobId },
-  } = req
+  try {
+    const {
+      user: { userId },
+      params: { id: jobId },
+    } = req
 
-  const job = await Job.findOneAndDelete({ _id: jobId, createdBy: userId })
-  if (!job) {
-    throw new NotFoundError("Job not found!")
+    const job = await Job.findOneAndDelete({ _id: jobId, createdBy: userId })
+    if (!job) {
+      throw new NotFoundError("Job not found!")
+    }
+
+    res.status(StatusCodes.OK).json({ msg: "Job Deleted Successfully!" })
+  } catch (error) {
+    if (error.name === "CastError") {
+      res.status(400).json({ msg: "Invalid request!" })
+    } else {
+      res.status(400).json({ msg: error.message })
+    }
   }
-
-  res.status(StatusCodes.OK).json({ msg: "Jon Deleted Successfully!" })
 }
 
 module.exports = { getAllJobs, getJob, createJob, updateJob, deleteJob }
