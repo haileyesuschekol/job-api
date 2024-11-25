84a5487251f8ed06cd5c977577e4a3aeee4ef0ba
Haileyesus Chekol <teamdevs7@gmail.com>
2024-11-25 16:52:38 +0000

    use security middlewares

--- app.js
+++ app.js
@@ -6,18 +6,30 @@ const connectDB = require("./db/connect")
 const jobRouter = require("./routes/jobs")
 const authRouter = require("./routes/auth")
 const auth = require("./middleware/authentication")
+
+//security packages
+const helmet = require("helmet")
+const cors = require("cors")
+const xss = require("xss-clean")
+const rateLimiter = require("express-rate-limit")
+
 // error handler
 const notFoundMiddleware = require("./middleware/not-found")
 const errorHandlerMiddleware = require("./middleware/error-handler")
 
-app.use(express.json())
 // extra packages
+app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+app.use(rateLimiter)
 
 // routes
 app.get("/", (req, res) => {
   res.send("jobs api")
 })
 
+//routes
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/jobs", auth, jobRouter)
 
